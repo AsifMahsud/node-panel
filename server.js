71867f86298ead6f331560aadc2d7610e3e78eaa
AsifMahsud <asifmahsud543@gmail.com>
2023-11-13 19:02:45 +0500

    Middleware for incoming signup requests

--- server.js
+++ server.js
@@ -2,87 +2,26 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan');
 const jwt = require('jsonwebtoken');
-const { validateSignupInput } = require('./validation')
+const signupRoutes = require('./routes/authentication');
+const { verifyToken } = require('./middlewares/auth');
+
 require('dotenv').config();
 
 
 const app = express()
 const port = process.env.port || 3000;
-const secretKey = process.env.SECRET_KEY;
 
 
 app.use(bodyParser.json())
 app.use(morgan('dev'))
+app.use('/protected', verifyToken);
+app.use('/admin', signupRoutes);
 
 
 app.get('/',(req,res)=>{
     res.send('Welcome')
 })
 
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
-
-    if (users[username] && users[username].password === password) {
-        const token = jwt.sign({ username, role: users[username].role }, secretKey);
-        res.json({ token });
-    } else {
-        res.status(401).json({ error: 'Invalid username or password' });
-    }
-  });
-
-  const users = {};
-
-  
-  app.post('/signup', (req, res) => {
-    const validationErrors = validateSignupInput(req.body);
-    if (validationErrors.length > 0) {
-        return res.status(400).json({ errors: validationErrors });
-      }
-
-    const {
-      first_name,
-      last_name,
-      creator_name,
-      email,
-      phone_number,
-      password,
-      password_confirmation,
-      accept_terms_conditions,
-    } = req.body;
-  
-    users[email] = {
-      first_name,
-      last_name,
-      creator_name,
-      email,
-      phone_number,
-      password,
-    };
-  
-    res.json({ message: 'Signup successful!' });
-  });
-  
-
-
-const verifyToken = (req, res, next) => {
-    const token = req.header('Authorization');
-    if (!token) {
-      return res.status(401).json({ error: 'Access denied. No token provided.' });
-    }
-  
-    try {
-      
-      const decoded = jwt.verify(token, secretKey);
-      req.user = decoded; 
-      next();
-    } catch (error) {
-      res.status(401).json({ error: 'Invalid token.' });
-    }
-  };
-  
-app.use('/protected', verifyToken);
-
-  
 app.get('/protected', (req, res) => {
     res.json({ message: 'Welcome to a protected route.' });
 });
