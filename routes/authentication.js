946e857529cc1932c1f97301ebd951a93fff2b91
AsifMahsud <asifmahsud543@gmail.com>
2023-11-13 21:18:43 +0500

    Social authentication included

--- routes/authentication.js
+++ routes/authentication.js
@@ -39,5 +39,4 @@ router.post('/signup', async(req, res) => {
   }
 });
 
-
 module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const bodyParser = require('body-parser')
 const morgan = require('morgan');
 const signupRoutes = require('./routes/authentication');
@@ -10,10 +12,37 @@ require('dotenv').config();
 const app = express()
 const port = process.env.port || 3000;
 
+app.use(require('express-session')({ secret: process.env.SECRET_KEY, resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
+const GOOGLE_CLIENT_ID = '265346054036-gmkifhn3v4kogem480ofg7a6n7198m4t.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = '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';
+passport.use(
+    new GoogleStrategy(
+      {
+        clientID: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
+        callbackURL: 'https://e133-122-129-66-106.ngrok-free.app/auth/google/callback',
+      },
+      (accessToken, refreshToken, profile, done) => {
+        // Store user information in the session
+        return done(null, profile);
+      }
+    )
+  );
+
+  passport.serializeUser((user, done) => {
+    done(null, user);
+  });
+  
+  // Deserialize user information from the session
+  passport.deserializeUser((obj, done) => {
+    done(null, obj);
+  });
 app.use(bodyParser.json())
 app.use(morgan('dev'))
-app.use('/protected', verifyToken);
+// app.use('/protected', verifyToken);
 app.use('/admin', signupRoutes);
 
 
@@ -21,10 +50,23 @@ app.get('/',(req,res)=>{
     res.send('Welcome')
 })
 
+// Initiate Google Sign-In
+app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+// Handle the Google Sign-In callback
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
+  res.redirect('/protected');
+});
+
 app.get('/protected', (req, res) => {
-    res.json({ message: 'Welcome to a protected route.' });
+    if (req.isAuthenticated()) {
+        res.json(req.user);
+      } else {
+        res.send('Failed authentication');
+      }
+    
 });
 
 app.listen(port,()=>{
-    console.log(`Serving at #{port}`)
+    console.log(`Serving at ${port}`)
 })
