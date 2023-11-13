8f66c911a92bacf3bd448a9217df3e94e15d804c
AsifMahsud <asifmahsud543@gmail.com>
2023-11-13 19:03:20 +0500

    Verify token middleware

---
+++ middleware/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const { SECRET_KEY } = process.env;
+
+const verifyToken = (req, res, next) => {
+  const token = req.header('Authorization');
+
+  if (!token) {
+    return res.status(401).json({ error: 'Access denied. No token provided.' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: 'Invalid token.' });
+  }
+};
+
+module.exports = {
+  verifyToken,
+};
